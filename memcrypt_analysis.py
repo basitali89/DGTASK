9c3075d722a900678e8e28eae256667403b16e4f
basitali89 <61506566+basitali89@users.noreply.github.com>
2021-08-02 17:09:37 +0100

    Function implementations: DecryptFile, KnownHeader

--- memcrypt_analysis.py
+++ memcrypt_analysis.py
@@ -1,6 +1,11 @@
+# Submitted By Basit Ali
+#Applicant : Junior Software Engineer (Python) MemCrypt Limited
+# Date: 02/07/2021
+#Email: basit@itpt.co.uk
 from Cryptodome.Cipher import AES
 from itertools import permutations
 import os
+from Cryptodome.Util.Padding import unpad
 
 #Below are header values for common file types.
 JPEG_HDR = b'\xFF\xD8\xFF\xE0'
@@ -11,8 +16,8 @@ PDF_HDR = b'%PDF-'
 def isIncremental(buffer): 
      """TO DO 1: Please implement a function which will:
 
-         1) Check if the 16-byte buffer contains incremental values (at 1-step intervals).
-         2) Return True if incremental values are detected. Otherwise, the function should return False.
+         1) Check if the 16-byte buffer contains incremental values (at 1-step intervals).✓
+         2) Return True if incremental values are detected. Otherwise, the function should return False.(As instructed)✓
 
       Examples of the many incremental values, found in memory_dump.bin, include:
 
@@ -24,24 +29,25 @@ def isIncremental(buffer):
       buffer -- the buffer to check for incremental values. 16-byte size buffers are passed in by default.
      """ 
      
-     for i in range(15):
-        
+     for i in range(len(buffer)-1):
         if buffer[i] + 1 != buffer[i + 1]:
-             #print(str(previous) + str(b) )
-             return False
-     return True   
+            continue
+        else:
+            return False 
+     return True
+       
      
 
  
 def decryptFile(candidates):
     """TO DO 2: Please implement a function which will:
         
-        1) Generate all permutations of candidate values (this has been done for you in the code below)
-        2) Test each candidate value against 'data\encrypted_file' using the Cryptodome.Cipher.AES decrypt* function. Ensure mode AES.MODE_CBC is used.
-        3) Check the header of each decryption attempt to determine if decryption was successful. The isKnownHeader() function can be used for this purpose. 
-        4) Output the correct key and IV (via a standard print statement) on successful decryption.
-        5) Write the decrypted file to the 'data' directory.
-        6) Consider extending the code to append the correct extension based on isKnownHeader() function match. (e.g. if the function determines the decrypted file to be JPG, add the .jpg extension).
+        1) Generate all permutations of candidate values (this has been done for you in the code below) ✓
+        2) Test each candidate value against 'data\encrypted_file' using the Cryptodome.Cipher.AES decrypt* function. Ensure mode AES.MODE_CBC is used. ✓
+        3) Check the header of each decryption attempt to determine if decryption was successful. The isKnownHeader() function can be used for this purpose. ✓
+        4) Output the correct key and IV (via a standard print statement) on successful decryption. ✓
+        5) Write the decrypted file to the 'data' directory. ✓
+        6) Consider extending the code to append the correct extension based on isKnownHeader() function match. (e.g. if the function determines the decrypted file to be JPG, add the .jpg extension). ✓
         
      *See https://pycryptodome.readthedocs.io/en/latest/src/cipher/aes.html for example usage.
 
@@ -49,7 +55,37 @@ def decryptFile(candidates):
      candidates -- The candidate keys and initialisation vectors (IVs) one wishes to test.
     """ 
     permu = list(permutations(candidates, 2)) #This function may be used to generate all permutations of candidate values.
-    pass
+    print("{n} key and IV combinations found \n".format(n = len(permu))) 
+
+    for i, pair in enumerate(permu):
+        print("Trying", "combination no:", i+1)
+        #initilisation -- AES.NEW(KEY, Mode, IV)
+        cipher = AES.new(pair[0], AES.MODE_CBC,pair[1]) 
+        #print(type(cipher))
+        encripted_file_path = "data\\encrypted_file"
+        with open(encripted_file_path, "rb") as ef:
+            edata = ef.read()
+            try:
+                #removing AES block padding
+                decriptedData = cipher.decrypt(edata)
+                plainText = unpad(decriptedData, AES.block_size)
+                res, filetype = isKnownHeader(plainText)
+                if res:
+
+                    print("Successful pair:\n\tKey: ",i,"     ", pair[0], "\n\tIV:", i,"     ", pair[1])
+                    
+                    decriptedfile = 'data\\decripted' + filetype
+                    with open(decriptedfile, "wb") as df:
+                        df.write(plainText)
+                        print("decripted file created @ root/data/" + decriptedfile)
+                    break
+                else:
+                    print("No known header matched, skipped...")
+                    
+                    
+            except :
+                print("combination failed")
+    
 
 def isKnownHeader(buffer):
     """This function performs analysis on the decrypted buffer to determine if it matches a known header (i.e. file type).
@@ -59,14 +95,16 @@ def isKnownHeader(buffer):
      buffer -- The buffer we wish to determine if decryption was successful.
     """ 
     if JPEG_HDR in buffer[0:len(JPEG_HDR)]:
-        return True
+        return True, '.jpg'
     if MS_OFFICE_HDR in buffer[0:len(MS_OFFICE_HDR)]:
-        return True
+        return True, 'office(TRY MANUALLY IN EXCEL, PP OR WORD)'
+        #TO DO open output file error handling or other method to verify
+        #https://filesignatures.net/index.php?page=search&search=504B030414000600&mode=SIG
     if PNG_HDR in buffer[0:len(PNG_HDR)]:
-        return True
+        return True, '.png'
     if PDF_HDR in buffer[0:len(PDF_HDR)]:
-        return True
-    return False
+        return True, '.pdf'
+    return False, 0
 
 def memoryAnalysis(file, offset):
     """This function iterates through the memory_dump.bin file and reads the content (buffer) of the file at 16-byte offsets.
@@ -84,16 +122,16 @@ def memoryAnalysis(file, offset):
         for i in range(0, filesize, offset):
             read = fh.read(offset)
 
-            if isIncremental(read) == False:
+            if isIncremental(read) != False:
                 candidates.append(read)
-    print(candidates)
+    #print(candidates) =  5 len for this case
 
     return candidates
 
 def main():
     #We begin by analysing the memory dump file. A list of candidate values will be returned by the function.
     candidates = memoryAnalysis(r"data\memory_dump.bin", 16)
-
+    print(len(candidates))
     #We then attempt to decrypt the encypted_file by trying all possible permutation of candidate values.
     decryptFile(candidates)
 
